0f6b8f707ce6b5cf2adad9f70fd199fd59227742
Wilmesmeier, Robert - ICN <Robert.Wilmesmeier@icn.de>
2024-01-18 09:40:23 +0100

    Sync

--- aspnet-core/src/Acme.BookStore.Application/Hubs/BookHub.cs
+++ aspnet-core/src/Acme.BookStore.Application/Hubs/BookHub.cs
@@ -11,29 +11,12 @@ using Volo.Abp.Domain.Entities;
 
 namespace Acme.BookStore.Hubs;
 
+// As suggested in https://github.com/abpframework/abp/issues/18793#issuecomment-1897958325, implementing the IUnitOfWorkEnabled interface could be a solution
+// Effectively, implementing the interface does not work, the database operations are not applied.
 public class BookHub : AbpHub<IBookHub>, IUnitOfWorkEnabled
 {
-    /*
-    // As suggested in https://github.com/abpframework/abp/issues/18793#issuecomment-1897958325, implementing the IUnitOfWorkEnabled interface could be a solution
-    // Effectively, implementing the interface does not work, the database operations are not applied.
-    public async override Task OnConnectedAsync()
-    {
-        IUnitOfWorkManager uowm = LazyServiceProvider.LazyGetRequiredService<IUnitOfWorkManager>();
-
-        using (IUnitOfWork uow = uowm.Begin())
-        {
-            IRepository<Book, Guid> bookRepository = uow.ServiceProvider.GetRequiredService<IRepository<Book, Guid>>();
-            if (await bookRepository.AnyAsync() == false)
-            {
-                throw new EntityNotFoundException(typeof(Book));
-            }
-        }
-        await base.OnConnectedAsync();
-    }
-    */
-
     [HubMethodName("CreateOrUpdateBook")]
-    public async Task CreateOrUpdateBookAsync(CreateUpdateBookDto createDto, Guid? id)
+    public virtual async Task CreateOrUpdateBookAsync(CreateUpdateBookDto createDto, Guid? id)
     {
         IBookAppService bookAppService = LazyServiceProvider.LazyGetRequiredService<IBookAppService>();
         BookDto bookDto = id.HasValue ? await bookAppService.UpdateAsync((Guid)id!, createDto) : await bookAppService.CreateAsync(createDto);
